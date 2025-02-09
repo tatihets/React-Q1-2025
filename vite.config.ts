547e6f87cd7fca93023277fda4b263724d4fed19
tatihets <bardlora3@gmail.com>
2025-02-09 21:26:59 +0100

    fix: vite.config fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
@@ -6,23 +6,19 @@ export default defineConfig({
   plugins: [react()],
   test: {
     setupFiles: './src/setup.ts',
-    environment: 'jsdom', // Use jsdom for React testing
-    globals: true, // Use global functions like describe, it, expect
-    include: ['src/**/*.{test,spec}.{ts,tsx}'], // Match test files
+    environment: 'jsdom',
+    globals: true,
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
     coverage: {
-      provider: 'v8', // Use v8 for coverage reporting
-      reporter: ['text', 'json', 'html'], // Output formats
-      include: ['**/*.tsx'], // Include only tsx files for coverage
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      include: ['**/*.tsx'],
       exclude: [
-        '**/node_modules/**', // Exclude node_modules
-        '**/*.test.tsx', // Exclude test files from coverage
-        '**/*.spec.tsx', // Exclude spec files from coverage
-        'src/__tests__/setup.ts', // Exclude setup files from coverage
-        'src/app/*', // Exclude App.tsx
+        '**/node_modules/**',
+        '**/*.test.tsx',
+        'src/setup.ts',
+        'src/app/*',
       ],
     },
-    // alias: {
-    //   '@assets': '/src/assets', // Resolve asset paths
-    // },
   },
 });
